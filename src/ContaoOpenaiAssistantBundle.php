8027c54516fa04afb585249124db1bcbc56e7c5f
JUHE IT-solutions <office@juhe-it-solutions.at>
2025-08-13 16:31:20 +0200

    Fix: use AbstractBundle::loadExtension with ContainerConfigurator to load services.yaml (post v1.0.3)

--- src/ContaoOpenaiAssistantBundle.php
+++ src/ContaoOpenaiAssistantBundle.php
@@ -12,9 +12,8 @@ declare(strict_types=1);
 
 namespace JuheItSolutions\ContaoOpenaiAssistant;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoOpenaiAssistantBundle extends AbstractBundle
@@ -29,12 +28,8 @@ class ContaoOpenaiAssistantBundle extends AbstractBundle
         return 'public';
     }
 
-    public function build(ContainerBuilder $container): void
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        parent::build($container);
-
-        // Load services configuration
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
-        $loader->load('services.yaml');
+        $container->import('../config/services.yaml');
     }
 }
